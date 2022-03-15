43918a71260d53432596fc51c6a87648978ed1b8
Archana Janarthanan <archanajl@gmail.com>
2022-03-15 01:44:56 +0000

    Exercise001 completed

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -1,30 +1,51 @@
 package com.techreturners.exercise001;
 
 import java.util.List;
+import java.util.Locale;
 
 public class Exercise001 {
     public String capitalizeWord(String word) {
         // Add your code here
-        return "";
+        return word.substring(0,1).toUpperCase() + word.substring(1);
     }
 
     public String generateInitials(String firstName, String lastName) {
         // Add your code here
-        return "";
+        return firstName.substring(0,1) + '.' +  lastName.charAt(0);
     }
 
     public double addVat(double originalPrice, double vatRate) {
         // Add your code here
-        return 0.0;
+        double calculatedVat = ((vatRate/100) * originalPrice)  + originalPrice;
+        return Math.floor(calculatedVat * 100)/100;
     }
 
     public String reverse(String sentence) {
         // Add your code here
-        return "";
+
+        StringBuilder reverseSentence = new StringBuilder();
+
+        String[] words = sentence.split("\\s+");
+        for (int i = words.length - 1 ; i >= 0; i--){
+            for (int j = words[i].length() - 1 ; j >= 0; j--) {
+                reverseSentence.append(words[i].charAt(j));
+            }
+            if (i != 0){
+                reverseSentence.append(" ");
+            }
+        }
+        return reverseSentence.toString();
     }
 
     public int countLinuxUsers(List<User> users) {
         // Add your code here
-        return 0;
+        int count = 0;
+            for (User i: users){
+            if (i.getType().equals("Linux")){
+                count++;
+            }
+
+        }
+        return count;
     }
 }

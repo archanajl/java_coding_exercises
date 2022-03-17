55dec80656d5fe2af1cb0064c5d4aa716f2718eb
Archana Janarthanan <archanajl@gmail.com>
2022-03-17 23:03:41 +0000

    Exercise005 completed

--- src/main/java/com/techreturners/exercise004/Exercise004.java
+++ src/main/java/com/techreturners/exercise004/Exercise004.java
@@ -1,19 +1,35 @@
 package com.techreturners.exercise004;
 
+import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class Exercise004 {
 
+    final LocalDateTime date_value;
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd.HH:mm:ss");
+
+
     public Exercise004(LocalDate date) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+        this.date_value = LocalDateTime.of(date, LocalTime.MIDNIGHT);
+    }
+
+    public Exercise004(String date) {
+        try {
+            this.date_value = LocalDateTime.parse(date, FORMATTER);
+        }catch (DateTimeParseException e){
+            throw new DateTimeException("Invalid Date");
+        }
     }
 
     public Exercise004(LocalDateTime dateTime) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+        this.date_value = dateTime;
     }
 
     public LocalDateTime getDateTime() {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+        return this.date_value.plusSeconds(1000000000);
     }
 }
--- src/test/java/com/techreturners/exercise004/Exercise004Test.java
+++ src/test/java/com/techreturners/exercise004/Exercise004Test.java
@@ -1,8 +1,8 @@
 package com.techreturners.exercise004;
 
-import org.junit.Ignore;
 import org.junit.Test;
-
+import java.time.DateTimeException;
+import static org.junit.Assert.assertThrows;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
@@ -12,32 +12,41 @@ import static org.junit.Assert.assertEquals;
 public class Exercise004Test {
 
     @Test
-    public void checkGetDateTimeWhenDateIsSpecified() {
+    public void checkGetDateTimeWhenDateIsSpecified()  {
 
         Exercise004 ex004 = new Exercise004(LocalDate.of(2021, Month.JULY, 19));
         LocalDateTime expected = LocalDateTime.of(2053, Month.MARCH, 27, 1, 46, 40);
-
         assertEquals(expected, ex004.getDateTime());
     }
 
-    @Ignore("You can remove this @ignore annotation to run the test")
     @Test
     public void checkGetDateTimeWhenBothDateAndTimeIsSpecified() {
 
         Exercise004 ex004 = new Exercise004(LocalDateTime.of(2021, Month.MARCH, 4, 23, 22, 0, 0));
         LocalDateTime expected = LocalDateTime.of(2052, Month.NOVEMBER, 11, 1, 8, 40);
-
         assertEquals(expected, ex004.getDateTime());
     }
 
-    @Ignore("You can remove this @ignore annotation to run the test")
     @Test
     public void checkGetDateTimeWhenBothDateAndTimeIsSpecifiedWithDayRollOver() {
 
         Exercise004 ex004 = new Exercise004(LocalDateTime.of(2021, Month.JANUARY, 24, 23, 59, 59, 0));
-        LocalDateTime expected = LocalDateTime.of(2052, Month.OCTOBER, 03, 1, 46, 39);
-
+        LocalDateTime expected = LocalDateTime.of(2052, Month.OCTOBER, 3, 1, 46, 39);
         assertEquals(expected, ex004.getDateTime());
     }
 
+    @Test
+    public void checkGetDateTimeWhenDateIsString()  {
+        Exercise004 ex004 = new Exercise004("2021-01-24.23:59:59");
+        LocalDateTime expected = LocalDateTime.of(2052, Month.OCTOBER, 3, 1, 46, 39);
+        assertEquals(expected, ex004.getDateTime());
+    }
+
+    @Test
+    public void checkGetDateTimeWhenDateAsStingIsEmpty() {
+        Exercise004 ex004;
+        DateTimeException exception =  assertThrows(DateTimeException.class, () -> new Exercise004(""));
+        assertEquals("Invalid Date", exception.getMessage());
+
+    }
 }
